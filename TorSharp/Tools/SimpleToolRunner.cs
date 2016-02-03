ed4c49ec2d81820de99c081fe9b81f45d6dd9884
James Skimming <JSkimming@users.noreply.github.com>
2016-02-03 22:35:19 +0000

    Added the simple tool runner
    The default tool runner uses several native win32 APIs which work well
    in a client environment, but can fail in secured/sandboxed server
    environments. The simple tool runner uses .NET APIs which in more
    situations.

---
+++ TorSharp/Tools/SimpleToolRunner.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Knapcode.TorSharp.Tools
+{
+    public class SimpleToolRunner : IToolRunner, IDisposable
+    {
+        private static readonly Task CompletedTask = Task.FromResult(0);
+        private bool _disposed;
+        private readonly ConcurrentBag<Process> _processes = new ConcurrentBag<Process>();
+
+        ~SimpleToolRunner()
+        {
+            Dispose(false);
+        }
+
+        public Task StartAsync(Tool tool)
+        {
+            // start the desired process
+            var arguments = string.Join(" ", tool.Settings.GetArguments(tool));
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = tool.ExecutablePath,
+                Arguments = arguments,
+                WorkingDirectory = tool.WorkingDirectory,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                WindowStyle = ProcessWindowStyle.Hidden,
+            };
+
+            Process process = Process.Start(startInfo);
+
+            if (process == null)
+            {
+                throw new TorSharpException($"Unable to start the process '{tool.ExecutablePath}'.");
+            }
+
+            _processes.Add(process);
+
+            return CompletedTask;
+        }
+
+        public void Stop()
+        {
+            while (!_processes.IsEmpty)
+            {
+                Process process;
+                _processes.TryTake(out process);
+
+                // If the process has not yet exited, ask nicely first.
+                if (!process.HasExited)
+                {
+                    if (process.CloseMainWindow())
+                    {
+                        process.WaitForExit(1000);
+                    }
+                }
+
+                // Still not exited? Then it's no more Mr Nice Guy.
+                if (!process.HasExited)
+                {
+                    process.Kill();
+                    process.WaitForExit(1000);
+                }
+            }
+        }
+
+        void IDisposable.Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing)
+            {
+                try
+                {
+                    Stop();
+                }
+                catch
+                {
+                    // Not much can be done, but must stop this bubbling.
+                }
+            }
+
+            // release any unmanaged objects
+            // set the object references to null
+
+            _disposed = true;
+        }
+    }
+}
