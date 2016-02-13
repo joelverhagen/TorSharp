29e2d7b81ca552c18be2a806574cccf28439b877
Joel Verhagen <jver@microsoft.com>
2016-02-12 18:23:23 -0800

    Add initial test (which always fails)

---
+++ TorSharp.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Knapcode.TorSharp.Tests")]
+[assembly: ComVisible(false)]
+[assembly: Guid("22c1582f-8723-428e-8098-0b179e9de361")]
---
+++ TorSharp.Tests/Sandbox.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Knapcode.TorSharp.Tests
+{
+    public class Sandbox
+    {
+        [Fact]
+        public void BadTest()
+        {
+            Assert.True(false, "This has to fail.");
+        }
+    }
+}
