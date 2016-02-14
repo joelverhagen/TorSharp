25975c93e45b0daaa0cd033391a52420738f8c14
Joel Verhagen <joel.verhagen@gmail.com>
2016-02-13 19:09:24 -0800

    Improve end to end tests

--- TorSharp.Tests/EndToEnd.cs
+++ TorSharp.Tests/EndToEnd.cs
@@ -5,11 +5,19 @@ using System.Net.Sockets;
 using System.Threading.Tasks;
 using Knapcode.TorSharp.Tests.TestSupport;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Knapcode.TorSharp.Tests
 {
     public class EndToEnd
     {
+        private readonly ITestOutputHelper _output;
+
+        public EndToEnd(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
         [Fact]
         public async Task EndToEnd_VirtualDesktopToolRunner()
         {
@@ -38,7 +46,66 @@ namespace Knapcode.TorSharp.Tests
             }
         }
 
-        private static async Task ExecuteEndToEndTestAsync(TorSharpSettings settings)
+        private async Task ExecuteEndToEndTestAsync(TorSharpSettings settings)
+        {
+            // Arrange
+            using (var proxy = new TorSharpProxy(settings))
+            {
+                // Act
+                await new TorSharpToolFetcher(settings, new HttpClient()).FetchAsync();
+                await proxy.ConfigureAndStartAsync();
+
+                // get the first identity
+                var ipA = await GetCurrentIpAddressAsync(proxy, settings);
+
+                // get the second identity
+                int attempts = 0;
+                IPAddress ipB = ipA;
+                while (attempts < 10 && ipA.Equals(ipB))
+                {
+                    attempts++;
+                    await proxy.GetNewIdentityAsync();
+                    ipB = await GetCurrentIpAddressAsync(proxy, settings);
+                }
+
+                _output.WriteLine($"Got IP B: {ipB}");
+
+                // Assert
+                Assert.Equal(AddressFamily.InterNetwork, ipA.AddressFamily);
+                Assert.Equal(AddressFamily.InterNetwork, ipB.AddressFamily);
+                Assert.NotEqual(ipA, ipB);
+            }
+        }
+
+        private async Task<IPAddress> GetCurrentIpAddressAsync(TorSharpProxy proxy, TorSharpSettings settings)
+        {
+            int attempts = 0;
+            while (true)
+            {
+                attempts++;
+
+                if (attempts > 0)
+                {
+                    await proxy.GetNewIdentityAsync();
+                }
+
+                try
+                {
+                    return await GetCurrentIpAddressAsync(settings);
+                }
+                catch(Exception e)
+                {
+                    if (attempts >= 3)
+                    {
+                        throw;
+                    }
+
+                    _output.WriteLine($"Get IP attempt {attempts} failed:{Environment.NewLine}{e}");
+                }
+            }
+        }
+
+        private async Task<IPAddress> GetCurrentIpAddressAsync(TorSharpSettings settings)
         {
             var handler = new HttpClientHandler
             {
@@ -46,34 +113,11 @@ namespace Knapcode.TorSharp.Tests
             };
 
             using (var httpClient = new HttpClient(handler))
-            using (var proxy = new TorSharpProxy(settings))
             {
-                // Act
-                // download tools
-                await new TorSharpToolFetcher(settings, new HttpClient()).FetchAsync();
-
-                // start the proxy
-                await proxy.ConfigureAndStartAsync();
-
-                // execute some HTTP requests
-                var unparsedIpA = await httpClient.GetStringAsync("http://ipv4.icanhazip.com");
-                var ipA = IPAddress.Parse(unparsedIpA.Trim());
-
-                // get a new identity
-                IPAddress ipB;
-                int attempts = 0;
-                do
-                {
-                    attempts++;
-                    await proxy.GetNewIdentityAsync();
-                    var unparsedIpB = await httpClient.GetStringAsync("http://ipv4.icanhazip.com");
-                    ipB = IPAddress.Parse(unparsedIpB.Trim());
-                }
-                while (Equals(ipA, ipB) && attempts < 10);
-
-                // Assert
-                Assert.Equal(AddressFamily.InterNetwork, ipA.AddressFamily);
-                Assert.Equal(AddressFamily.InterNetwork, ipB.AddressFamily);
+                httpClient.Timeout = TimeSpan.FromSeconds(10);
+                var ip = (await httpClient.GetStringAsync("http://ipv4.icanhazip.com")).Trim();
+                _output.WriteLine($"Get IP succeeded: {ip}");
+                return IPAddress.Parse(ip);
             }
         }
     }
