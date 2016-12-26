6c957906a529d9f5321dffde9504a95089e8066e
Joel Verhagen <joel.verhagen@gmail.com>
2016-12-26 10:44:45 -0800

    Switch from legacy .csproj to new PackageReference-style .csproj

--- samples/Knapcode.TorSharp.Sandbox/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Knapcode.TorSharp.Sandbox")]
-[assembly: ComVisible(false)]
-[assembly: Guid("b1b318a7-a085-401e-96d3-320fe1066c45")]
