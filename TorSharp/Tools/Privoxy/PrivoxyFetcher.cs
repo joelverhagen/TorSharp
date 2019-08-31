51299f0d6103f7a1d3a89fea0df25da40c95a351
Joel Verhagen <joel.verhagen@gmail.com>
2019-08-31 14:51:48 -0700

    Revert "Remove dependency on System.ServiceModel"
    This reverts commit cda754d5b1c8f2ea89ffed086aed154cd1f1e6c0.

--- TorSharp/Tools/Privoxy/PrivoxyFetcher.cs
+++ TorSharp/Tools/Privoxy/PrivoxyFetcher.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.ServiceModel.Syndication;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Xml;
-using System.Xml.Linq;
 
 namespace Knapcode.TorSharp.Tools.Privoxy
 {
@@ -128,7 +128,7 @@ namespace Knapcode.TorSharp.Tools.Privoxy
                 {
                     var streamReader = new StreamReader(stream);
                     var xmlReader = XmlReader.Create(streamReader);
-                    syndicationFeed = SyndicationFeed.Load(xmlReader, baseUrl);
+                    syndicationFeed = SyndicationFeed.Load(xmlReader);
                     if (syndicationFeed == null)
                     {
                         return null;
@@ -169,102 +169,6 @@ namespace Knapcode.TorSharp.Tools.Privoxy
         private static bool TitleEndsWithPrivoxyZip(SyndicationItem item)
         {
             return Regex.IsMatch(item.Title.Text, FileNamePattern, RegexOptions.IgnoreCase);
-        }
-
-        private class SyndicationFeed
-        {
-            public SyndicationFeed(IReadOnlyList<SyndicationItem> items)
-            {
-                Items = items;
-            }
-
-            public IReadOnlyList<SyndicationItem> Items { get; }
-
-            public static SyndicationFeed Load(XmlReader reader, Uri url)
-            {
-                var doc = XDocument.Load(reader);
-
-                Guard(doc.Root.Name.LocalName == "rss", $"The root element should be 'rss', in {url.AbsoluteUri}");
-                var rss = doc.Root;
-                var channel = GetElement(rss, "channel", url);
-
-                var syndicationItems = new List<SyndicationItem>();
-                foreach (var item in channel.Elements("item"))
-                {
-                    var title = GetElement(item, "title", url);
-                    var link = GetElement(item, "link", url);
-
-                    Guard(
-                        Uri.TryCreate(link.Value, UriKind.Absolute, out var itemUrl),
-                        $"The link value was not an absolute URL at path {GetDisplayPath(item)}, in {url.AbsoluteUri}.");
-
-                    syndicationItems.Add(new SyndicationItem(
-                        new SyndicationItemTitle(title.Value),
-                        new[] { new SyndicationItemLink(itemUrl) }));
-                }
-
-                return new SyndicationFeed(syndicationItems);
-            }
-
-            private static XElement GetElement(XElement parent, string name, Uri url)
-            {
-                var el = parent.Element(name);
-                Guard(el != null, $"No '{name}' element was found at path {GetDisplayPath(parent)}, in {url.AbsoluteUri}.");
-                return el;
-            }
-
-            private static void Guard(bool value, string message)
-            {
-                if (!value)
-                {
-                    throw new TorSharpException(message);
-                }
-            }
-
-            private static string GetDisplayPath(XElement node)
-            {
-                var output = node.Name.ToString();
-                var current = node;
-                while (current.Parent != null)
-                {
-                    current = current.Parent;
-                    output = current.Name + " -> " + output;
-                }
-
-                return output;
-            }
-        }
-
-        private class SyndicationItem
-        {
-            public SyndicationItem(SyndicationItemTitle title, IReadOnlyList<SyndicationItemLink> links)
-            {
-                Title = title;
-                Links = links;
-            }
-
-            public SyndicationItemTitle Title { get; }
-            public IReadOnlyList<SyndicationItemLink> Links { get; }
-        }
-
-        public class SyndicationItemTitle
-        {
-            public SyndicationItemTitle(string text)
-            {
-                Text = text;
-            }
-
-            public string Text { get; }
-        }
-
-        public class SyndicationItemLink
-        {
-            public SyndicationItemLink(Uri uri)
-            {
-                Uri = uri;
-            }
-
-            public Uri Uri { get; }
         }
     }
 }
