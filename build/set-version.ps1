12240ad12a0d7c61b47678b5dda3dabb6a609569
Joel Verhagen <joel.verhagen@gmail.com>
2020-11-28 13:36:54 -0800

    Use the head ref and then ref

--- build/set-version.ps1
+++ build/set-version.ps1
@@ -1,13 +1,19 @@
 Write-Host "[ Determining release version ]" -ForegroundColor DarkGreen
 $version = (Get-Content (Join-Path $PSScriptRoot "version.txt")).Trim()
 
-if ($env:GITHUB_REF) {
-    $branchPart = "." + $env:GITHUB_REF.Split('/')[-1]
+if ($env:GITHUB_HEAD_REF) {
+    $ref = $env:GITHUB_HEAD_REF
+} elseif ($env:GITHUB_REF) {
+    $ref = $env:GITHUB_REF
+}
+
+if ($ref) {
+    $dotBranch = "." + $ref.Split('/')[-1]
 }
 
 [int]$buildNumber = $null
 if ([int]::TryParse($env:GITHUB_RUN_NUMBER, [ref]$buildNumber) ) {
-    $version = $version + "-ci$branchPart.$buildNumber"
+    $version = $version + "-ci$dotBranch.$buildNumber"
 }
 
 $env:TorSharpVersion = $version
