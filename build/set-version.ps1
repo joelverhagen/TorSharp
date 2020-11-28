0f95de96f7744b38342397811a15db1fe040f06c
Joel Verhagen <joel.verhagen@gmail.com>
2020-11-28 15:26:43 -0800

    Get branch name and run number from GitHub environment (#62)
    Also, push to GPR and upload artifacts

--- build/set-version.ps1
+++ build/set-version.ps1
@@ -1,9 +1,19 @@
 Write-Host "[ Determining release version ]" -ForegroundColor DarkGreen
 $version = (Get-Content (Join-Path $PSScriptRoot "version.txt")).Trim()
 
+if ($env:GITHUB_HEAD_REF) {
+    $ref = $env:GITHUB_HEAD_REF
+} elseif ($env:GITHUB_REF) {
+    $ref = $env:GITHUB_REF
+}
+
+if ($ref) {
+    $dotBranch = "." + $ref.Split('/')[-1]
+}
+
 [int]$buildNumber = $null
-if ([int]::TryParse($env:APPVEYOR_BUILD_NUMBER, [ref]$buildNumber) ) {
-    $version = $version + "-ci-" + $buildNumber
+if ([int]::TryParse($env:GITHUB_RUN_NUMBER, [ref]$buildNumber) ) {
+    $version = $version + "-ci$dotBranch.$buildNumber"
 }
 
 $env:TorSharpVersion = $version
