57ea05030363b49f5b53f0c366be484b498a8615
Joel Verhagen <joel.verhagen@gmail.com>
2020-11-28 13:23:14 -0800

    Get branch name and run number from GitHub environment

--- build/set-version.ps1
+++ build/set-version.ps1
@@ -1,9 +1,13 @@
 Write-Host "[ Determining release version ]" -ForegroundColor DarkGreen
 $version = (Get-Content (Join-Path $PSScriptRoot "version.txt")).Trim()
 
+if ($env:GITHUB_REF) {
+    $branchPart = "." + $env:GITHUB_REF.Split('/')[-1]
+}
+
 [int]$buildNumber = $null
-if ([int]::TryParse($env:APPVEYOR_BUILD_NUMBER, [ref]$buildNumber) ) {
-    $version = $version + "-ci-" + $buildNumber
+if ([int]::TryParse($env:GITHUB_RUN_NUMBER, [ref]$buildNumber) ) {
+    $version = $version + "-ci$branchPart.$buildNumber"
 }
 
 $env:TorSharpVersion = $version
