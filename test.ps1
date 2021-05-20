dd4ddbd0233ba6853d9b093d7305242f72cb903a
Daniel Cazzulino <daniel@cazzulino.com>
2021-05-20 18:37:35 -0300

    Add latest supported .NET Framework
    Fixes #76

--- test.ps1
+++ test.ps1
@@ -5,7 +5,7 @@ if ($env:APPVEYOR) {
     $options = "-appveyor"
 }
 
-$netFrameworkTestAssembly = "artifacts\TorSharp\TorSharp.Tests\bin\Release\net452\Knapcode.TorSharp.Tests.dll"
+$netFrameworkTestAssembly = "artifacts\TorSharp\TorSharp.Tests\bin\Release\net472\Knapcode.TorSharp.Tests.dll"
 
 if (Test-Path $netFrameworkTestAssembly) {
     Write-Host "[ Running .NET Framework tests in 32-bit ]" -ForegroundColor DarkGreen
@@ -15,10 +15,10 @@ if (Test-Path $netFrameworkTestAssembly) {
     Exec { & $xunit64 $netFrameworkTestAssembly $options }
 } else {
 	Write-Host "[ Building .NET Framework tests with .NET CLI ]" -ForegroundColor DarkGreen
-	Exec { dotnet build test\TorSharp.Tests --framework net452 }
+	Exec { dotnet build test\TorSharp.Tests --framework net472 }
 
 	Write-Host "[ Running .NET Framework tests with .NET CLI ]" -ForegroundColor DarkGreen
-	Exec { dotnet test test\TorSharp.Tests --no-build --framework net452 --verbosity normal }
+	Exec { dotnet test test\TorSharp.Tests --no-build --framework net472 --verbosity normal }
 }
 
 Write-Host "[ Building .NET Core tests with .NET CLI ]" -ForegroundColor DarkGreen
