52c0ae110270769ebc6a46b9ae1f9bb513b7cd9c
Joel Verhagen <joel.verhagen@gmail.com>
2022-12-23 11:04:21 -0500

    Clean up a bit and more to prerelease xRetry for strong name

--- test.ps1
+++ test.ps1
@@ -1,18 +1,13 @@
 . (Join-Path $PSScriptRoot "build\common.ps1")
 
-$options = ""
-if ($env:APPVEYOR) {
-    $options = "-appveyor"
-}
-
 $netFrameworkTestAssembly = "artifacts\TorSharp\TorSharp.Tests\bin\Release\net472\Knapcode.TorSharp.Tests.dll"
 
 if (Test-Path $netFrameworkTestAssembly) {
     Write-Host "[ Running .NET Framework tests in 32-bit ]" -ForegroundColor DarkGreen
-    Exec { & $xunit32 $netFrameworkTestAssembly $options }
+    Exec { & $xunit32 $netFrameworkTestAssembly }
 
     Write-Host "[ Running .NET Framework tests in 64-bit ]" -ForegroundColor DarkGreen
-    Exec { & $xunit64 $netFrameworkTestAssembly $options }
+    Exec { & $xunit64 $netFrameworkTestAssembly }
 } else {
 	Write-Host "[ Building .NET Framework tests with .NET CLI ]" -ForegroundColor DarkGreen
 	Exec { dotnet build test\TorSharp.Tests --framework net472 }
