ba82d24ceaf7e07fb9b4a0a89ddb3364f2ad528a
Joel Verhagen <joel.verhagen@gmail.com>
2023-10-24 10:47:47 -0400

    Use the local source code for Docker sample

--- samples/Docker/run.ps1
+++ samples/Docker/run.ps1
@@ -1,1 +1,10 @@
-docker build . -t docker-repro --no-cache --progress plain
+Push-Location
+try {
+    $root = [System.IO.Path]::GetFullPath((Join-Path $PSScriptRoot ../..))
+    Set-Location $root
+    docker build . -t docker-repro --no-cache --progress plain -f (Join-Path $PSScriptRoot "Dockerfile")    
+}
+catch {
+    Pop-Location
+}
+
